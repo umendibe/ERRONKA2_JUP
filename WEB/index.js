3856d71f1448ddce90dd640a0b4bbb525db6875e
Urko <ikumendibe25@lhusurbil.eus>
2025-10-29 14:10:27 +0100

    Botoiak hasten

---
+++ WEB/index.js
@@ -0,0 +1,9 @@
+function aurrera_pelikulak() {
+    // Hurrengo pelikula kargatu
+    document.getElementById('blackphone2').src = 'img2/pelikulak2/maspalomas_handia.png';
+}
+
+function atzera_pelikulak() {
+    // Aurreko pelikula kargatu
+    document.getElementById('blackphone2').src = 'img2/pelikulak2/blackphone2_handia.png';
+}
