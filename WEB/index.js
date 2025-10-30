8310e2947495079a253246f3df4bf265a584d06b
Urko <ikumendibe25@lhusurbil.eus>
2025-10-30 13:34:47 +0100

    header aldatzen: menu desplegablea egiten

--- WEB/index.js
+++ WEB/index.js
@@ -1,9 +1,9 @@
 const images = [
-  { src: 'img2/pelikulak2/blackphone2_handia.png', text: 'BLACKPHONE 2' },
-  { src: 'img2/pelikulak2/maspalomas_handia.png', text: 'MASPALOMAS' },
-  { src: 'img2/pelikulak2/crepusculo_handia.jpg', text: 'CREPUSCULO: Nada Volverá a Ser Lo Mismo' },
-  { src: 'img2/pelikulak2/los_domingos_handia.jpg', text: 'LOS DOMINGOS' },
-  { src: 'img2/pelikulak2/tom_y_jerry_Handia.jpg', text: 'TOM Y JERRY: Aventura En El Tiempo' }
+    { src: 'img2/pelikulak2/blackphone2_handia.png', text: 'BLACKPHONE 2' },
+    { src: 'img2/pelikulak2/maspalomas_handia.png', text: 'MASPALOMAS' },
+    { src: 'img2/pelikulak2/crepusculo_handia.jpg', text: 'CREPUSCULO: Nada Volverá a Ser Lo Mismo' },
+    { src: 'img2/pelikulak2/los_domingos_handia.jpg', text: 'LOS DOMINGOS' },
+    { src: 'img2/pelikulak2/tom_y_jerry_Handia.jpg', text: 'TOM Y JERRY: Aventura En El Tiempo' }
 ];
 
 
@@ -11,30 +11,58 @@ let index = 0;
 const img = document.getElementById('blackphone2');
 
 function eguneratuArgazkia() {
-  if (!img) return;
-  img.src = images[index].src;
-  tituluak.textContent = images[index].text;
-
-  img.classList.add('fade-out');
-
-  setTimeout(() => {
+    if (!img) return;
     img.src = images[index].src;
-    img.classList.remove('fade-out');
-  }, 200);
+    tituluak.textContent = images[index].text;
+
+    img.classList.add('fade-out');
+
+    setTimeout(() => {
+        img.src = images[index].src;
+        img.classList.remove('fade-out');
+    }, 200);
 }
 
 function hurrengoIrudia() {
-  if (index < images.length - 1) {
-    index++;
-    eguneratuArgazkia();
-  }
+    if (index < images.length - 1) {
+        index++;
+        eguneratuArgazkia();
+    }
 }
 
 function atzeraIrudia() {
-  if (index > 0) {
-    index--;
-    eguneratuArgazkia();
-  }
+    if (index > 0) {
+        index--;
+        eguneratuArgazkia();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', eguneratuArgazkia);
+
+function desplegable() {
+
+        const submenu = document.getElementById('submenu');
+        const menuIcon = document.querySelector('#menuButton img');
+
+        const isOpen = submenu.style.display === 'flex';
+
+        if (isOpen) {
+            submenu.style.display = 'none';
+            menuIcon.style.transform = 'rotate(0deg)'; 
+        } else {
+            submenu.style.display = 'flex';
+            menuIcon.style.transform = 'rotate(-90deg)'; 
+        }
+    
+    window.addEventListener('click', (event) => {
+        const menuButton = document.getElementById('menuButton');
+        const submenu = document.getElementById('submenu');
+        const menuIcon = document.querySelector('#menuButton img');
+
+        if (!menuButton.contains(event.target)) {
+            submenu.style.display = 'none';
+            menuIcon.style.transform = 'rotate(0deg)';
+        }
+    });
+
+}
