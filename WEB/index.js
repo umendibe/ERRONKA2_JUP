3365b31b77fd86f3ff79cdca4b6706ef8e0dcd0b
Urko <ikumendibe25@lhusurbil.eus>
2025-10-30 11:37:41 +0100

    Botoiak lantzen eta pelikulen portada gehiago jartzen

--- WEB/index.js
+++ WEB/index.js
@@ -1,9 +1,31 @@
-function aurrera_pelikulak() {
-    // Hurrengo pelikula kargatu
-    document.getElementById('blackphone2').src = 'img2/pelikulak2/maspalomas_handia.png';
+const images = [
+  'img2/pelikulak2/blackphone2_handia.png',
+  'img2/pelikulak2/maspalomas_handia.png',
+  'img2/pelikulak2/crepusculo_handia.jpg',
+  'img2/pelikulak2/los_domingos_handia.jpg',
+  'img2/pelikulak2/tom_y_jerry_Handia.jpg'
+];
+
+let index = 0;
+const img = document.getElementById('blackphone2');
+
+function eguneratuArgazkia() {
+  if (!img) return;
+  img.src = images[index];
 }
 
-function atzera_pelikulak() {
-    // Aurreko pelikula kargatu
-    document.getElementById('blackphone2').src = 'img2/pelikulak2/blackphone2_handia.png';
+function hurrengoIrudia() {
+  if (index < images.length - 1) {
+    index++;
+    eguneratuArgazkia();
+  }
 }
+
+function atzeraIrudia() {
+  if (index > 0) {
+    index--;
+    eguneratuArgazkia();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', eguneratuArgazkia);
