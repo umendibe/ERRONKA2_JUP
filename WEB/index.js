8a10312cbe4b7ecb14f8e91bb496675091f2725f
Urko <ikumendibe25@lhusurbil.eus>
2025-10-30 12:16:22 +0100

    Botoiak bukatzen eta animazioak jartzen js-ekin

--- WEB/index.js
+++ WEB/index.js
@@ -1,17 +1,26 @@
 const images = [
-  'img2/pelikulak2/blackphone2_handia.png',
-  'img2/pelikulak2/maspalomas_handia.png',
-  'img2/pelikulak2/crepusculo_handia.jpg',
-  'img2/pelikulak2/los_domingos_handia.jpg',
-  'img2/pelikulak2/tom_y_jerry_Handia.jpg'
+  { src: 'img2/pelikulak2/blackphone2_handia.png', text: 'BLACKPHONE 2' },
+  { src: 'img2/pelikulak2/maspalomas_handia.png', text: 'MASPALOMAS' },
+  { src: 'img2/pelikulak2/crepusculo_handia.jpg', text: 'CREPUSCULO: Nada Volverá a Ser Lo Mismo' },
+  { src: 'img2/pelikulak2/los_domingos_handia.jpg', text: 'LOS DOMINGOS' },
+  { src: 'img2/pelikulak2/tom_y_jerry_Handia.jpg', text: 'TOM Y JERRY: Aventura En El Tiempo' }
 ];
+
 
 let index = 0;
 const img = document.getElementById('blackphone2');
 
 function eguneratuArgazkia() {
   if (!img) return;
-  img.src = images[index];
+  img.src = images[index].src;
+  tituluak.textContent = images[index].text;
+
+  img.classList.add('fade-out');
+
+  setTimeout(() => {
+    img.src = images[index].src;
+    img.classList.remove('fade-out');
+  }, 200);
 }
 
 function hurrengoIrudia() {
