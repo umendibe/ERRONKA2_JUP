ab4c268439814a4c5ad584601615b076417229df
Juan <ikjortiz25@lhusurbil.eus>
2025-10-30 11:38:30 +0100

    data aldatzen

--- WEB/saskia.js
+++ WEB/saskia.js
@@ -5,3 +5,13 @@ document.getElementById("popup").style.display = "block";
 function itxiDialogoa(){
     document.getElementById("popup").style.display = "none";
 }
+const dataEguna = document.getElementById("data");
+dataEguna.addEventListener('input', function() {
+ const data = new Date (this.value);
+ const eguna = data.getDay();
+ if(eguna !== 0 && eguna !== 6);{
+    this.value = '';
+ alert("Aukeratu asteburuko egun bat.");
+ }
+
+});
