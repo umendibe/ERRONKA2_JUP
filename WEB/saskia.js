53a31f383ab8e6dfe970c5a4eb5410515a834df7
Juan <ikjortiz25@lhusurbil.eus>
2025-10-31 09:15:39 +0100

    js

--- WEB/saskia.js
+++ WEB/saskia.js
@@ -1,35 +1,43 @@
+function irekiDialogoa() {
+  document.getElementById("popup").style.display = "block";
+}
 
-function irekiDialogoa(){
-document.getElementById("popup").style.display = "block";
+function itxiDialogoa() {
+  document.getElementById("popup").style.display = "none";
 }
-function itxiDialogoa(){
-    document.getElementById("popup").style.display = "none";
-}
+
+
 const dataEguna = document.getElementById("data");
+const txartela = document.getElementById("txartela");
+
 dataEguna.addEventListener('input', function() {
- const data = new Date (this.value);
- const eguna = data.getDay();
-if (eguna !== 6 && eguna !== 0) {
-    this.value = ''; 
-    alert("Aukeratu asteburuko egun bat."); 
-  }else if(eguna==0){
-    this.value = '';
-    alert("ikuslearen eguna da");
+  const data = new Date(this.value);
+  const eguna = data.getDay(); 
+
+   if(eguna !==0 && eguna !== 6){
+      alert("Bakarrik asteburuetan irikitzen dugu.");
+      this.value = "";
+  if (txartela.value === "deskontua4") {
+    if (eguna !== 0) { 
+      alert("Ikuslearen eguna bakarrik igandean egin daiteke!");
+      this.value = "";
+    }}
   }
 });
-const txartela = document.getElementById("txartela")
-const txartelakodea = document.getElementById("txartelakodea")
+
+const txartelakodea = document.getElementById("txartelakodea");
 const kodeaInput = document.getElementById('kodea');
-const bidali = document.getElementById("bidalikodea")
+const bidali = document.getElementById("bidalikodea");
+
 txartela.addEventListener('change', function() {
- if (txartela.value !== "deskontua4" && txartela.value !== "deskontua5") {
+  if (txartela.value !== "deskontua4" && txartela.value !== "deskontua5") {
     txartelakodea.style.display = 'block';
-  }  else {
+  } else {
     txartelakodea.style.display = 'none'; 
     kodeaInput.value = '';                 
   }
-})
-bidali.addEventListener('click', function(){
-    txartelakodea.style.display = 'none';
-})
+});
 
+bidali.addEventListener('click', function(){
+  txartelakodea.style.display = 'none';
+});
