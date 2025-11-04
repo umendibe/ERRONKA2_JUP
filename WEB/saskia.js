ee20dba227f5370dfcadb52e635ce9c85f8bb738
Juan <ikjortiz@gmail.com>
2025-11-04 11:38:58 +0100

    amaituta

--- WEB/saskia.js
+++ WEB/saskia.js
@@ -10,49 +10,49 @@ function itxiDialogoa() {
 const dataEguna = document.getElementById("data");
 const txartela = document.getElementById("txartela");
 
-dataEguna.addEventListener('input', function() {
+dataEguna.addEventListener('input', function () {
   const data = new Date(this.value);
-  const eguna = data.getDay(); 
+  const eguna = data.getDay();
 
   if (txartela.value === "deskontua4") {
-    if (eguna !== 0) { 
+    if (eguna !== 0) {
       alert("Ikuslearen eguna bakarrik igandean egin daiteke!");
       this.value = "";
-    }}
-  if(eguna !==0 && eguna !== 6){
-      alert("Bakarrik asteburuetan irekitzen dugu.");
-      this.value = "";
     }
+  }
+  if (eguna !== 0 && eguna !== 6) {
+    alert("Bakarrik asteburuetan irekitzen dugu.");
+    this.value = "";
+  }
 });
 
 const txartelakodea = document.getElementById("txartelakodea");
 const kodeaInput = document.getElementById('kodea');
 const bidali = document.getElementById("bidalikodea");
 
-txartela.addEventListener('change', function() {
+txartela.addEventListener('change', function () {
   if (txartela.value !== "deskontua4" && txartela.value !== "deskontua5") {
     txartelakodea.style.display = 'block';
   } else {
-    txartelakodea.style.display = 'none'; 
-    kodeaInput.value = '';                 
+    txartelakodea.style.display = 'none';
+    kodeaInput.value = '';
   }
 });
 
-bidali.addEventListener('click', function(){
+bidali.addEventListener('click', function () {
   txartelakodea.style.display = 'none';
 });
 
 function balidatuKodea() {
   const kodea = document.getElementById("bidalikodea");
- 
-  kodea.addEventListener("click",  () => {
+
+  kodea.addEventListener("click", () => {
     alert("DESKONTUA DUZU!");
   });
- }
+} balidatuKodea();
+const pertsonaKopurua = document.getElementById("perkop");
 
- const pertsonaKopurua = document.getElementById("perkop");
-
-pertsonaKopurua.addEventListener('input', function() {
+pertsonaKopurua.addEventListener('input', function () {
   const kopurua = parseInt(this.value);
   if (kopurua < 1 || kopurua > 4) {
     alert("Mesedez, sartu 1 eta 4 arteko pertsona kopurua.");
