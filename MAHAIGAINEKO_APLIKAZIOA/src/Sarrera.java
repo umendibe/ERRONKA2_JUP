060412cf02ffa7dc05f9046591be2dbd8e1e96d4
Urko <ikumendibe25@lhusurbil.eus>
2025-11-12 09:01:49 +0100

    Sarrera.java-ko dokumentazioa

--- MAHAIGAINEKO_APLIKAZIOA/src/Sarrera.java
+++ MAHAIGAINEKO_APLIKAZIOA/src/Sarrera.java
@@ -1,11 +1,11 @@
 public class Sarrera {
-
-    private String pelikula;
-    private String eguna;
-    private int pertsonaKopurua;
-    private double prezioa;
-    private double guztira;
-
+    /** BARIABLE PUBLIKOAK (Atributuak) */
+    private String pelikula; /** Pelikula izena*/
+    private String eguna; /** Eguna*/
+    private int pertsonaKopurua; /** Pertsona kopurua*/
+    private double prezioa; /** Prezioa*/
+    private double guztira; /** Guztira*/
+    /** Objektuaren eraikitzailea */
     public Sarrera(String pelikula, String eguna, int pertsonaKopurua, double prezioa) {
         this.pelikula = pelikula;
         this.eguna = eguna;
@@ -14,27 +14,32 @@ public class Sarrera {
         this.guztira = pertsonaKopurua * prezioa;
     }
 
-    // Getters
+    /** Informazioa lortzeko metodoak */
+
+    /** Pelikula izena lortzeko metodoa */
     public String getPelikula() {
         return pelikula;
     }
 
+    /** Eguna lortzeko metodoa */
     public String getEguna() {
         return eguna;
     }
 
+    /** Pertsona kopurua lortzeko metodoa */
     public int getPertsonaKopurua() {
         return pertsonaKopurua;
     }
 
+    /** Prezioa lortzeko metodoa */
     public double getPrezioa() {
         return prezioa;
     }
-
+    /** Guztira ordaindu beharreko metodoa */
     public double getGuztira() {
         return guztira;
     }
-
+    /** toString metodoa: Sarrera objektuaren informazioa itzultzen du (String batean bihurtu) */
     @Override
     public String toString() {
         return "EGUNA: " + eguna + "\nPELIKULA: " + pelikula + "\nPERTSONA KOPURUA: " + pertsonaKopurua + "\nORDAINDU = " + guztira + "euro";
