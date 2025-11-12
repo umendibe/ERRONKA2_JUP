3061c302d30eed2d4b41cc5e8331c095c9c07a10
ikjortiz25 <ikjortiz25@example.com>
2025-11-12 09:09:32 +0100

    kommit

--- MAHAIGAINEKO_APLIKAZIOA/src/Sarrera.java
+++ MAHAIGAINEKO_APLIKAZIOA/src/Sarrera.java
@@ -5,7 +5,10 @@ public class Sarrera {
     private int pertsonaKopurua;
     private double prezioa;
     private double guztira;
-
+    /**
+     * Sarrera klasearen eraikitzailea (constructor).
+     * Eraikitzaile honek sarreraren informazioa jasotzen du eta guztira kalkulatzen du automatikoki.
+     */
     public Sarrera(String pelikula, String eguna, int pertsonaKopurua, double prezioa) {
         this.pelikula = pelikula;
         this.eguna = eguna;
@@ -14,27 +17,39 @@ public class Sarrera {
         this.guztira = pertsonaKopurua * prezioa;
     }
 
-    // Getters
+    /** 
+     * Pelikularen izena itzultzen du.
+     */
     public String getPelikula() {
         return pelikula;
     }
-
+     /** 
+     * Egunaren izena itzultzen du.
+     */
     public String getEguna() {
         return eguna;
     }
-
+     /** 
+     * Pertsona kopurua itzultzen du.
+     */
     public int getPertsonaKopurua() {
         return pertsonaKopurua;
     }
-
+     /** 
+     * Prezioa pertsona bakoitzeko itzultzen du.
+     */
     public double getPrezioa() {
         return prezioa;
     }
-
+     /** 
+     * Guztira ordaindu beharreko zenbatekoa itzultzen du.
+     */
     public double getGuztira() {
         return guztira;
     }
-
+    /** 
+     * Erosketaren informazioa testu formatuan itzultzen du.
+     */
     @Override
     public String toString() {
         return "EGUNA: " + eguna + "\nPELIKULA: " + pelikula + "\nPERTSONA KOPURUA: " + pertsonaKopurua + "\nORDAINDU = " + guztira + "euro";
